37cf8dc94792b91209e7f7e3428f9597720814f0
Conor Doyle <conor.doyle@gmail.com>
2018-10-01 20:13:04 -0500

    PS-2018.2.4 <cdoyle@cdoyle-osx.local Create markdown.xml, IntelliLang.xml, _@user_Material Theme - Darker.icls, _@user_All hallow_s eve.icls, textmate.xml, project.default.xml, diff.xml, _@user_Solarized Light.icls, Default.xml, _@user_Monokai.icls, file.template.settings.xml, find.xml, SQL Script.sql, _@user_Cobalt.icls, code.style.schemes.xml, _@user_WarmNeon.icls, Default _3_.xml, _@user_Darcula.icls, path.macros.xml, github_settings.xml, _@user_Blackboard.icls, _@user_Twilight.icls, Obsidian.icls, Default.xml, PHP.xml, Default _2_.xml, _@user_VibrantInk.icls, _@user_Railscasts.icls, editor.codeinsight.xml, PHP Setter Method.php, Mac OS X 10_5_ copy 0.xml, keymap.xml, editor.rich.copy.xml, Material Theme - Darker copy.icls, editor.xml, applicationLibraries.xml, webServers.xml, debugger.xml, Zend Class.php, baseRefactoring.xml, _@user_Material Theme - Default.icls, ide.general.xml, _@user_Default.icls, Material Theme - Darker copy copy.icls, git.xml, other.xml, xpath.xml, ui.lnf.xml, filetypes.xml, php.xml, _@user_Material Theme - Lighter.icls, xsl.xml, watcherDefaultTasks.xml, colors.scheme.xml, External Tools.xml, notifications.xml, databaseDrivers.xml, Snippet.xml, PHP File Header.php, laf.xml, Default _1_.xml, Mac OS X 10_5_ copy.xml, web-browsers.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, usageView.xml, Mac OS X copy.xml, _@user_Solarized Dark.icls

---
+++ fileTemplates/Zend Class.php
@@ -0,0 +1,11 @@
+<?php
+#parse("PHP File Header.php")
+ 
+class ${NAME} extends ${EXTENDS} {
+
+    public function init()
+    {
+    
+    }
+
+}
---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @author ${USER} <conor.doyle@nerdery.com>
+ * @package ${PACKAGE}
+ * @subpackage ${SUBPACKAGE}
+ * 
+ * @version ${VERSION} 
+ */
